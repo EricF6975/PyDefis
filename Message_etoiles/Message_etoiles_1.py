79ce6b4c778f3fa12c3e64762d5de1c1d756fa5d
EricF6975 <eric.feuerstein@ecomail.fr>
2025-09-18 16:12:40 +0200

    Suppression de quelques commentaires

--- Message_etoiles/Message_etoiles_1.py
+++ Message_etoiles/Message_etoiles_1.py
@@ -38,29 +38,12 @@ for file in glob.glob(path):
 
     for i in range(0,len(pixel_value)):
          pixel_sum.append(sum(pixel_value[i]))
-        #  if sum(pixel_value[i])>0:
-        #       print(f"index:{i} : {sum(pixel_value[i])}")
 
-
-    #print(f"somme pixel: {pixel_sum}")
-
-    #liste_toutes_img.append(list(img.getdata()))
     liste_toutes_img.append(pixel_sum)
 
     
 
-#print(len(liste_toutes_img[0]))
 print(f"long liste toutes img {len(liste_toutes_img)}")
-
-#print(f"somme:{sum()}")
-
-#for i in range(0,len(liste_toutes_img[0])):
-
-#test
-# liste_toutes_img[10][10]=20
-# liste_toutes_img[2][15]=47
-# liste_toutes_img[12][1200]=47
-
 
 
 df = pd.DataFrame(liste_toutes_img)
@@ -77,10 +60,7 @@ print(df.info())
 #liste_max=[]
 liste_h_etoiles=[]
 for i in range(0,640000):
-#for i in range(0,2000):
-     #liste_max.appen(df[i].max())
-     #liste_min.append(df[i].min())
-     #print(f"max :{df[i].max()} / min:{df[i].min()}")
+
      if df[i].max() > df[i].min():
             liste_h_etoiles.append(i)
             #print(calc_coord(i))
